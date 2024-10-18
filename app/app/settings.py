8e38aa04db8294ebbc5f6d8b6e9882578370fa78
huari1969 <koen.vandenberghe@just.fgov.be>
2024-10-18 11:02:23 +0200

    added GitHub actions noqa e501 aanpassing 2 no white space

--- app/app/settings.py
+++ app/app/settings.py
@@ -85,10 +85,10 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    { 'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },  # noqa: E501
-    { 'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },  # noqa: E501
-    { 'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', },  # noqa: E501
-    { 'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', },  # noqa: E501
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },  # noqa: E501
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },  # noqa: E501
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', },  # noqa: E501
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', },  # noqa: E501
 ]
 
 
